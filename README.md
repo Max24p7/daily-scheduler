b1e74a6de82de72bf104626d4769b150bdb8b838
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-07-19 19:28:38 -0500

    Added readme and deleted TODO comments. added and edited commented code.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # daily-scheduler
+
+## Description
+
+This project is a daily scheduler created utilizing day.js to retrive current time.  The scheduler uses different colors to display urgency of daily tasks.
+
+## Installation
+
+Run the index.html file in a browser window or go to the github pages link here: <a href="">Daily Scheduler</a>
+
+## Usage
+
+Using the scheduler is simple.  Begin by building out your schedule for the day by typing out tasks in each time block. You will have to click save on each individual time block in order to save what has been written.  As your day progresses, the time blocks will change in color indicating urgency of task completion. (Grey = Past tasks -- Green = Future tasks -- Red = Current tasks). 
+
+![Screenshot of page](screenshot.PNG)
+
+
+## Credits
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+
+https://www.youtube.com/watch?v=vzGzys3enCE&t=4s
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
--- script.js
+++ script.js
@@ -1,57 +1,37 @@
+//CREATE TIMER AT TOP OF PAGE (DAY, MM DD YYYY, TIME)
 const today = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
 const time = today.toLocaleDateString('en-US', options);
 document.getElementById('currentDay').textContent = time;
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
   $(".saveBtn").on("click", function () {
-    const timeID = $(this).parent().attr("id"); // get the id of the time-block element
-    const value = $(this).siblings("textarea").val(); // get the value of the textarea element
-    localStorage.setItem(timeID, value); // store the value using the id as a key
+    const timeID = $(this).parent().attr("id");
+    const value = $(this).siblings("textarea").val(); //retrive the value of the textarea element
+    localStorage.setItem(timeID, value); //store the value
   });
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
     
-  $(".time-block").each(function () {
+  $(".time-block").each(function () { //function for painting the hours by past/present/future.
     const hour = parseInt($(this).attr("id").split("-")[1]); // get the hour from the id and parse it as a number
-    const currentHour = dayjs().hour(); // get the current hour in 24-hour time using Day.js
-    if (hour < currentHour) {
-        $(this).addClass("past"); // add past class if hour is less than current hour
-        $(this).removeClass("present"); // remove present class if present
-        $(this).removeClass("future"); // remove future class if present
-    } else if (hour === currentHour) {
-        $(this).addClass("present"); // add present class if hour is equal to current hour
-        $(this).removeClass("past"); // remove past class if present
-        $(this).removeClass("future"); // remove future class if present
-    } else {
-        $(this).addClass("future"); // add future class if hour is greater than current hour
-        $(this).removeClass("past"); // remove past class if present
-        $(this).removeClass("present"); // remove present class if present
+    const currentHour = dayjs().hour(); //get the current time using day.js
+    if (hour < currentHour) {            //if the hour is in the past, make the background color gray
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future"); 
+    } else if (hour === currentHour) {  //if the hour is current, make the background color red
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+    } else {                            //if the hour is in the future, make the background color green
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present"); 
     }
   });
 
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
   $(".time-block").each(function () {
     const timeID = $(this).attr("id"); // get the id of each element
-    const value = localStorage.getItem(timeID); // retrieve the value using the id as a key
-    $(this).children("textarea").val(value); // set the value of the textarea element
+    const value = localStorage.getItem(timeID); //retrieves previous saved info from local storage
+    $(this).children("textarea").val(value); //set the value from storage
   });
 });
